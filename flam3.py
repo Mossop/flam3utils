39686b9e5ca63cafd7c20b7856ced7aa03231fa0
Dave Townsend <dtownsend@oxymoronical.com>
2013-03-26 22:16:35 -0700

    Fix specifying quality in config files.

--- flam3.py
+++ flam3.py
@@ -241,7 +241,7 @@ class Flame:
       self.element.setAttribute("scale", "%f" % scale)
 
     if options.quality is not None:
-      self.element.setAttribute("quality", "%d" % options.quality)
+      self.element.setAttribute("quality", "%d" % int(options.quality))
 
     renderer = Flam3Renderer(options)
     display = ConsoleDisplay()
